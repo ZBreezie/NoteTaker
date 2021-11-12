c563023b6a04133805dd8898a3f3eb4c8a1433af
ZBreezie <zpmc55@yahoo.com>
2021-11-12 18:14:47 -0500

    got posts to write to json file/consolidated code

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { notes } = require('./db/db');
 const path = require('path');
-
+const fs = require('fs');
 
 const app = express();
 
@@ -13,31 +13,38 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 app.use(express.static('public'));
 
+// Function to create new note
+function createNote(body, notesArray) {
+    console.log(body);
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+}
+
+// Get note json data
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+// Post json data
 app.post('/api/notes', (req, res) => {
     console.log(req.body);
-    res.json(req.body);
-  });
+    req.body.id = notes.length.toString();
+    const note = createNote(req.body, notes);
+    res.json(note);
+});
 
 
-
-
-
-// Specify to /notes
+// Route to /notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
-// Default to main page
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-// });
 
 // Default to main page
 app.get('*', (req, res) => {
