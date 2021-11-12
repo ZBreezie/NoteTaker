4c35728311ab2d6b03d0e1fba1cf72dae1e0db05
ZBreezie <zpmc55@yahoo.com>
2021-11-12 17:15:01 -0500

    consolidated routes, added api get route

--- Develop/server.js
+++ Develop/server.js
@@ -1,21 +1,49 @@
 const express = require('express');
-
-// Attach html routing
-const htmlRoute = require('./routes/htmlRoute');
+const { notes } = require('./db/db');
+const path = require('path');
 
 
 const app = express();
 
 // Set port to 3001
 const PORT = process.env.PORT || 3001;
-app.use(express.urlencoded({ extended: true }));
 
-// Parse data as json
+// Parse incoming string or array data
+// Parse json data
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 app.use(express.static('public'));
-app.use('/', htmlRoute);
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+  });
+
+
+
+
+
+// Specify to /notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// Default to main page
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+// });
+
+// Default to main page
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`Connected to server on port ${PORT}!`);
